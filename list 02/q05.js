885d756151a0ec6aaa905916282224a4bf0d7785
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-22 09:27:52 -0300

    comentarios potencia de 2

--- list 02/q05.js
+++ list 02/q05.js
@@ -1,12 +1,13 @@
 // potencia natural de 2
 
+// funcao que recebe um numero como argumento e retorna 2 elevado a ele
 const pot = (n) => {
-
+    // caso base: n^0 = 1
     if (n == 0) return 1
-
+    // passo indutivo: 2^n = 2 * 2^n-1
     else return 2*pot(n-1)
 }
-
+// chamada da funcao
 const resultado = pot(10)
-
+// saida
 console.log(resultado)
