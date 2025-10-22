fac2e231359249b1e8c52c652d26962ae0ce203c
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:28:03 -0300

    comentarios citacao

--- list 01/q10.js
+++ list 01/q10.js
@@ -1,7 +1,8 @@
 // citação bibliografica
 
+// funcao currificada que recebe nome e sobrenome e formata como uma citacao bibliografica
 const citacao = (nome) => (sobrenome) => `${sobrenome}, ${nome}.`
-
+// chamada da funcao
 const resultado = citacao("Victor")("Santos")
-
+// saida
 console.log(resultado)
