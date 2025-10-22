7ac606b7bc794fc7c74a66b869ec47c471d3ddac
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:09:57 -0300

    comentarios distancia entre pontos

--- list 01/q05.js
+++ list 01/q05.js
@@ -1,7 +1,8 @@
 // distancia euclidiana
 
+// funcao currificada que tem como parametros dois pares ordenados e calcula a distancia entre eles
 const distancia = (x1, y1) => (x2, y2) => (((x1-x2)**2)+((y1-y2)**2))**0.5
-
+// chamada da funcao
 const resultado = distancia(1,1)(1,5)
-
+// saida
 console.log(resultado)
