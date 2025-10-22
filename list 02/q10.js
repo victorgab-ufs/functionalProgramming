af1ad55ddba5fb8d84123a4f325b3d0824fd4c47
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-22 09:59:26 -0300

    comentarios inverter palavra

--- list 02/q10.js
+++ list 02/q10.js
@@ -1,12 +1,13 @@
 // inverter palavra
 
+// funcao que recebe uma string como argumento e retorna ela invertida
 const inverter = (str) => {
-
+    // caso base: se o tamanho da string for 0, retorna um vazio, ja que nao tem o que inverter
     if (str.length == 0) return ''
-    
+    // separa o primeiro caractere do restante da string
     const [x, ...xs] = str
-
+    // passo indutivo: retorna o resto da palavra para ser invertida e coloca o primeiro termo atual na ultima posicao
     return inverter(xs)+[x]
 }
-
+// saida
 console.log(inverter('socorram'))
