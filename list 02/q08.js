404643885a342b1848eb4ff3096b5402d6fe5faa
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-22 09:39:31 -0300

    comentarios tamanho str

--- list 02/q08.js
+++ list 02/q08.js
@@ -1,12 +1,13 @@
 // tamanho de string
 
+// funcao que recebe uma string como argumento e retorna o tamanho dela sem uso do .length
 const tamanho = (str) => {
-
+    // separa o primeiro caractere da lista (string) do resto
     const [x, ...xs] = str
-
+    // caso base: se o resto separado eh um vazio, so ha um caractere, logo retorna 1
     if (xs == '') return 1
-
+    // passo indutivo: soma 1 (x) e retorna recursivamente a funcao o resto da palavra (sem o primeiro caractere)
     else return 1 + tamanho(xs)
 }
-
+// saida
 console.log(tamanho('cinco'))
