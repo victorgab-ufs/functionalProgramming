3bb69c24503f4624822deedfe60b63dfa64037a6
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:26:50 -0300

    comentarios ou exclusivo

--- list 01/q09.js
+++ list 01/q09.js
@@ -1,10 +1,11 @@
 // ou-exclusivo
 
+// funcao que recebe dois valores (boolean -> true, false) e retorna o ou-exclusivo (xor)
 const ouExclusivo = (valor1, valor2) => {
-
+    // calculo do ou exclusivo (disjuncao) conjuncao (negacao_da_conjuncao) -> (AvB) ^ ~(A^B)
     return (valor1 || valor2) && (!(valor1 && valor2))
 }
-
+// chamada da funcao
 const resultado = ouExclusivo(false, false)
-
+// saida
 console.log(resultado)
