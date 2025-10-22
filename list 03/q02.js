aff9eb7bb0662604f8379ded1f5280821f3383c8
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:45:16 -0300

    comentarios ultimo elemento

--- list 03/q02.js
+++ list 03/q02.js
@@ -1,13 +1,15 @@
 // último elemento
 
+// funcao que recebe lista como argumento e retorna o ultimo elemento (indice = tamanho_da_lista-1)
 const ultimo = (lista) => `O último elemento é ${lista[lista.length-1]}`
-
+// funcao que recebe lista como argumento e retorna penultimo elemento da lista
 const penultimo = (lista) => `O penúltimo elemento é ${lista[lista.length-2]}`
 
+// funcao que chama as funcoes ultimo e penultimo
 const resultado = (lista = [1, 2, 2, 40, 20]) => {
 
     return `${ultimo(lista)}\n${penultimo(lista)}`
 
 }
-
+//saida
 console.log(resultado())
