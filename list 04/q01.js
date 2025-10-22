adbc5c8add756660f996e4c0e7d911618ab45b0d
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:18:03 -0300

    comentarios eliminar elemento

--- list 04/q01.js
+++ list 04/q01.js
@@ -1,10 +1,13 @@
 // eliminar elemento
 
+// funcao que recebe como argumentos um elemento e uma lista e retorna uma copia da lista sem esse elemento escolhido
 const eliminar = (elemento, lista) => {
 
-    return [...lista].filter(x => x != elemento)
+    // ...spread -> aceita qualquer numero de argumentos (dentro de colchetes cria uma copia da lista)
+    // .filter(argumento => comparacao(os que obedecerem permanecem na lista))
+    return [...lista].filter(x => x != elemento) // filtra os elementos que sao diferentes daquele escolhido
 }
-
+// chamada da funcao
 const resultado = eliminar(7, [1, 7, 2, 7, 3])
-
+// saida
 console.log(resultado)
