93583bc01a613c2e67b0bb155ffb4da1a6fe73fc
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:07:05 -0300

    comentarios n-esimo termo

--- list 02/q01.js
+++ list 02/q01.js
@@ -1,15 +1,16 @@
 // n-ésimo termo
 
+// funcao que recebe um numero e retorna o n-esimo termo da sequencia: 3, 6, 12, 24...
 const termo = (num) => {
-
+    // caso base: se o numero atual for 1, retorna 3 (primeiro elemento da sequencia)
     if (num == 1) return 3
 
     else {
-
+        // passo indutivo, o numero seguinte sempre eh o dobro do numero anterior e por ai vai...
         return 2*termo(num-1)
     }
 }
-
+// chamada da funcao
 const resultado = termo(5)
-
+// saida
 console.log(resultado)
