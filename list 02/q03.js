6231b578bdbb7af83d30b4e13fe21122126af9d2
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:14:35 -0300

    comentarios soma de 1 a n

--- list 02/q03.js
+++ list 02/q03.js
@@ -1,15 +1,17 @@
 // soma de naturais
 
+// funcao que recebe um numero como argumento e retorna a soma de 1 ate o numero
 const soma = (num) => {
-
+    // caso base: se o numero atual for 0, retorna 0 (adicionando-o a soma e finalizando a recursao)
     if (num == 0) return 0
 
     else {
-
-        return soma(num-1)+num
+        // passo indutivo -> soma ate n e: n + soma dos elementos de n-1 ate 1
+        return soma(num-1)+num // chamada recursiva
     }
 }
 
+// chamada da funcao
 const resultado = soma(5)
-
+// saida
 console.log(resultado)
