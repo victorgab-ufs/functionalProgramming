329f9290d146d6bfa168fc4eeae4e4eea2410a90
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-22 09:25:21 -0300

    comentarios fatorial

--- list 02/q04.js
+++ list 02/q04.js
@@ -1,21 +1,22 @@
 // fatorial
 
+// funcao que recebe um numero como argumento e retorna o fatorial dele (n!)
 const fatorial = (num) => {
-
-    if (num == 0) return 0
+    // caso base 1: 0! = 1
+    if (num == 0) return 1
 
     else {
-        
+        // caso base 2: 1! = 1
         if (num == 1) return 1
 
         else {
-
+        // passo indutivo (recursao): fatorial de um numero -> n! = n * (n-1)!
         return num*fatorial(num-1)
         }
     }
 
 }
-
+// chamada da funcao
 const resultado = fatorial(5)
-
+// saida
 console.log(resultado)
