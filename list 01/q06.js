a7d76be9f5e617c519553a2bed2d4f8f38fd6766
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:12:21 -0300

    comentarios igualdade

--- list 01/q06.js
+++ list 01/q06.js
@@ -1,17 +1,18 @@
 // quantidade de iguais
 
+// funcao que recebe tres numeros
 const igualdade = (num1, num2, num3) => {
-
+    // checa se os numeros sao todos iguais (3 iguais)
     if (num1 == num2 && num2 == num3) return "Todos os números são iguais"
 
     else{
-
+        // checa se todos sao diferentes (0 iguais)
         if (num1 != num2 && num2 != num3 && num3 != num1) return "Não há números iguais"
-
+        // caso que sobrou: dois sao iguais
         else return "2 números são iguais"
     }
 }
-
+// chamada da funcao
 const resultado = igualdade(0,1,3)
-
+// saida
 console.log(resultado)
