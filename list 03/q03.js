034a344759fe73d0d8500efcad5a2636cfa46109
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:42:54 -0300

    comentarios tamanho lista

--- list 03/q03.js
+++ list 03/q03.js
@@ -1,7 +1,8 @@
 // número de elementos
 
+// funcao que recebe lista como argumento e retorna a quantidade de elementos presentes nela
 const tamanho = (lista) => `A lista contém ${lista.length} elementos`
-
+// chamada da funcao
 const resultado = tamanho([10, 30, 50])
-
+// saida
 console.log(resultado)
