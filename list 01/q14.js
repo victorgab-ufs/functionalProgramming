61b43c9dc5e496629ba91b23f257211c4b83d9ef
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:02:46 -0300

    comentarios algarismos por extenso

--- list 01/q14.js
+++ list 01/q14.js
@@ -1,13 +1,14 @@
 // escrever algarismo por extenso
 
+// funcao que recebe um algarismo e retorna sua representacao por extenso
 const algarismo = (num) => {
-
+// lista com o nome dos algarismos (zero a nove)
 const numeros = Object.freeze(['zero','one','two','three','four','five','six','seven','eight','nine'])
-
+// retorna o nome do elemento inserido no input
 return numeros[num]
 
 }
-
+// chamada da funcao
 const resultado = algarismo(4)
-
+// saida
 console.log(resultado)
