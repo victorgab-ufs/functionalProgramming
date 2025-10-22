1ed8141b555693ec5c02482f1019bba43e9d7ec3
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-22 09:35:04 -0300

    comentarios quociente

--- list 02/q07.js
+++ list 02/q07.js
@@ -1,20 +1,21 @@
 // quociente entre 2 numeros
 
+// funcao auxiliar que recebe dois numeros como argumento e retorna o resto da divisao do primeiro pelo segundo
 const resto = (dividendo, divisor) => {
-
+    // caso base: se o dividendo for menor, ele mesmo eh o resto da divisao
     if (dividendo < divisor) return dividendo
 
     else {
-        
+        // passo indutivo: eh tirado o divisor do dividendo ate que esse seja menor, caindo no caso base
         return resto(dividendo - divisor, divisor)
     }
 }
-
+// funcao que recebe dois numeros como argumento e retorna o quociente da divisao do primeiro pelo segundo
 const quociente = (dividendo, divisor) => {
-
+    // calculo do quociente -> q = (dividendo - resto) / divisor
     return (dividendo - resto(dividendo, divisor))/divisor
 }
-
+// chamada da funcao
 const resultado = quociente(17, 2)
-
+// saida
 console.log(resultado)
