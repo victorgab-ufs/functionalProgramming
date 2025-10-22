f7261f82973b635c7a24661561c8dcbfcf9e5e7a
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:08:24 -0300

    comentarios tipo triangulo

--- list 01/q04.js
+++ list 01/q04.js
@@ -1,17 +1,18 @@
 // classificar triângulo
 
+// funcao que recebe como parametros os tres lados de um triangulo
 const triangulo = (lado1, lado2, lado3) => {
-
+    // checa se os tres lados sao iguais -> equilatero
     if (lado1 == lado2 && lado2 == lado3) return "o triangulo e equilatero"
     else {
-
+        // checa se os tres lados sao diferentes -> escaleno
         if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) return "o triangulo e escaleno"
-
+        // ultimo caso -> eh isosceles
         else return "o triangulo e isosceles"
     }
 
 }
-
+// chamada da funcao
 const resultado = triangulo(2,3,1)
-
+// saida
 console.log(resultado)
