5fd3f1b0f7e337d996e5c4c00206ff953fd3e0c3
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:03:34 -0300

    comentarios area circulo

--- list 01/q02.js
+++ list 01/q02.js
@@ -1,7 +1,8 @@
 // área da circunferência
 
+// definicao da funcao 
 const area = (raio, pi=3.14) => pi*(raio**2)
 
-const resultado = area(10)
+const resultado = area(10) // chamada da funcao, pi ja esta definido, portanto nao eh necessario chama-lo, a menos que seja necessario trocar seu valor
 
 console.log(resultado)
