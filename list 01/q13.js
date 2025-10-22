28f15959db4f8ea83f4775fe863f2c8ed9d1de33
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:00:55 -0300

    comentarios velocidade

--- list 01/q13.js
+++ list 01/q13.js
@@ -1,10 +1,11 @@
 // velocidade
 
-const velocidade = (tempo) => (posicao) => {
+// funcao currificada que recebe tempo e deslocamento e retorna o valor da velocidade
+const velocidade = (tempo) => (deslocamento) => {
 
-    return `A velocidade é: ${posicao/tempo} m/s`
+    return `A velocidade é: ${deslocamento/tempo} m/s`
 }
-
+// chamada da funcao
 const resultado = velocidade(10)(100)
-
+// saida
 console.log(resultado)
