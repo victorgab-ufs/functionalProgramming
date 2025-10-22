f58bc537c77519de5ed471849892fe5ba1be8c9b
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:02:02 -0300

    comentarios area

--- list 01/q01.js
+++ list 01/q01.js
@@ -1,7 +1,10 @@
 // área do retângulo
 
-const area = (base, altura) => base*altura
+// definicao da funcao de calculo da area de retangulo currificada
+             // | parametros |     | corpo da funcao |
+const area = (base) => (altura) => base*altura
 
+// chamada da funcao
 const resultado = area(2)(2)
 
 console.log(resultado)
