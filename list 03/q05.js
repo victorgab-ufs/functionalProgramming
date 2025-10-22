944c6415448c4065fb1c982bf80a9d69a65b2472
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 23:32:18 -0300

    comentarios

--- list 03/q05.js
+++ list 03/q05.js
@@ -2,17 +2,21 @@
 
 // (1+2+3+4+5+6+7+8+9+10)² - (1²+2²+3²+4²+5²+6²+7²+8²+9²+10²)
 
+// lista com os numeros naturais de 1 a 10
 const naturais = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+// funcao que recebe uma lista como argumento e retorna o resultado da expressao apresentada acima
 const dif = (lista) => {
-
+    // funcao soma que sera utilizada no reduce
     const sum = (acc, x) => acc + x
+    // quadrado da soma de 1 a 10 (reduce faz a soma dos elementos da lista)
     const quadSum = (lista.reduce(sum, 0))**2
+    // soma dos quadrados (map gera uma copia da lista com todos elementos ao quadrado e reduce soma os elementos dessa copia gerada)
     const sumQuad = (lista.map(n => n**2)).reduce(sum, 0)
-
+    // subtracao requisitada
     return quadSum - sumQuad
 }
-
+// chamada da funcao
 const resultado = dif(naturais)
-
+// saida
 console.log(resultado)
