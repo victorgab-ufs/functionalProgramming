9db989a343cc0f24ebf28e99f8d93b4fcbacc35a
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 22:20:50 -0300

    comentarios quarta potencia

--- list 01/q08.js
+++ list 01/q08.js
@@ -1,12 +1,13 @@
 // quarta potência
 
+// funcao auxiliar que recebe um numero e calcula seu quadrado
 const quadrado = (num) => num**2
-
+// funcao que recebe um numero e calcula a quarta potencia
 const quarta = (num) => {
-
-    return quadrado(quadrado(3))
+    // utilizacao da funcao auxiliar para calcular o quadrado de um numero e o quadrado do resultado obtido
+    return quadrado(quadrado(num))
 }
-
+// chamada da funcao
 const resultado = quarta(2)
-
+// saida
 console.log(resultado)
